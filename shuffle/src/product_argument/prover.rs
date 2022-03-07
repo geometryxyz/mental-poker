fc0dec84f6244b2e76b119e94c9491c2598f1b64
Andrija <akinovak@gmail.com>
2022-03-07 21:55:02 +0100

    full product argument
    Co-authored-by: Nicolas Mohnblatt <nmohnblatt@users.noreply.github.com>

---
+++ shuffle/src/product_argument/prover.rs
@@ -0,0 +1,106 @@
+use super::{Statement, Parameters, Witness, proof::Proof};
+
+use crate::{
+    utils::{
+        ScalarSampler, RandomSampler, PedersenCommitment, HomomorphicCommitment, HadamardProduct, HadamardProductCalculator
+    }, 
+    product_argument::*,
+};
+
+use ark_ec::{ProjectiveCurve};
+use ark_ff::{One};
+use rand::Rng;
+
+
+pub struct Prover<'a, C>
+where 
+    C: ProjectiveCurve,
+{
+    parameters: &'a Parameters<'a, C>,
+    statement: &'a Statement<'a, C>,
+    witness: &'a Witness<'a, C>, 
+}
+
+impl<'a, C: ProjectiveCurve> Prover<'a, C> {
+    pub fn new(
+        parameters: &'a Parameters<'a, C>,
+        statement: &'a Statement<'a, C>,
+        witness: &'a Witness<'a, C>
+    ) -> Self {
+
+        Self {
+            parameters,
+            statement, 
+            witness
+        }
+    }
+
+    pub fn prove<R: Rng>(&self, rng: &mut R) -> Proof<C> {
+
+        let s = ScalarSampler::<C>::sample_element(rng);
+        let product_along_rows = self.witness.matrix_a.iter().fold(vec![C::ScalarField::one(); self.parameters.n], |x, y| {
+            HadamardProductCalculator::<C>::scalars_by_scalars(&x, &y).unwrap()
+        });
+
+        let b_commit = PedersenCommitment::<C>::commit_vector(self.parameters.commit_key, &product_along_rows, s);
+
+        // Engage in Hadamard Product Argument for b_commit and the `product_along_rows` as its witness:
+        // This will show that each entry in `product_along_rows` is computed correctly
+        let hadamard_product_parameters = hadamard_product_argument::Parameters::<C>::new(
+            self.parameters.m,
+            self.parameters.n,
+            &self.parameters.commit_key
+        );
+        
+        let hadamard_product_statement = hadamard_product_argument::Statement::new(
+            &self.statement.commitments_to_a,
+            b_commit
+        );
+
+        let hadamard_product_witness = hadamard_product_argument::Witness::<C>::new(
+            self.witness.matrix_a,
+            self.witness.randoms_for_a_commit,
+            &product_along_rows,
+            s
+        );
+
+        let hadamard_product_prover = hadamard_product_argument::prover::Prover::new(
+            &hadamard_product_parameters,
+            &hadamard_product_statement,
+            &hadamard_product_witness
+        );
+
+        let hadamard_product_proof = hadamard_product_prover.prove(rng);
+        
+        // Engage in single value product argument for b_commit and b as a statement:
+        // This will show that our claimed product b is indeed the product of the values in
+        // `product_along_rows`
+        let single_value_product_parameters = single_value_product_argument::Parameters::<C>::new(
+            self.parameters.n, 
+            &self.parameters.commit_key
+        );
+        let single_value_product_witness = single_value_product_argument::Witness::<C>::new(
+            &product_along_rows,
+            &s
+        );
+
+        let single_value_product_statement = single_value_product_argument::Statement::<C>::new(
+            b_commit, 
+            self.statement.b
+        );
+        
+        let single_value_prover = single_value_product_argument::prover::Prover::new(
+            &single_value_product_parameters, 
+            &single_value_product_statement,
+            &single_value_product_witness, 
+        );
+
+        let single_value_proof = single_value_prover.prove(rng);
+
+        Proof {
+            b_commit, 
+            hadamard_product_proof,
+            single_value_proof
+        }
+    }
+}
---
+++ shuffle/src/product_argument/tests.rs
@@ -0,0 +1,82 @@
+#[cfg(test)]
+
+mod test {
+
+    use crate::{
+        utils::{RandomSampler, ScalarSampler, HomomorphicCommitment, PedersenCommitment}
+    };
+    use ark_ec::{ProjectiveCurve};
+    use ark_ff::One;
+    use starknet_curve;
+    use ark_std::rand::{thread_rng};
+    use crate::product_argument::{Witness, prover::Prover, Parameters, Statement};
+    use rand::Rng;
+
+
+    fn generate_commit_key<R: Rng, C: ProjectiveCurve>(public_randomess: &mut R, len: &usize) -> Vec<C::Affine> {
+        let mut commit_key = Vec::with_capacity(len + 1);
+        let mut base = C::rand(public_randomess);
+        for _ in 0..len + 1 {
+            commit_key.push(base.into_affine());
+            base.double_in_place();
+        }
+        commit_key
+    }
+
+    #[test] 
+    fn test_complete_product_argument() {
+        let m = 4;
+        let n = 13;
+
+        let rng = &mut thread_rng();
+
+        let commit_key = generate_commit_key::<_, starknet_curve::Projective>(rng, &n);
+
+        let random_scalars = ScalarSampler::<starknet_curve::Projective>::sample_vector(rng, m*n);
+        let a_chunks = random_scalars.chunks(n).map(|c| c.to_vec()).collect::<Vec<_>>();
+
+        let r = ScalarSampler::<starknet_curve::Projective>::sample_vector(rng, a_chunks.len());
+        let a_commits = a_chunks.iter().zip(r.iter()).map(|(a_chunk, &random)|{
+            PedersenCommitment::commit_vector(&commit_key, a_chunk, random)
+        }).collect::<Vec<starknet_curve::Projective>>();
+        
+        let raw_prod = random_scalars.iter().fold(starknet_curve::Fr::one(), |x, y| {
+            x * y
+        });
+
+        let proof_parameters = Parameters::<starknet_curve::Projective>::new(m, n, &commit_key);
+        let statement = Statement::new(&a_commits, raw_prod);
+
+        let valid_witness = Witness::<starknet_curve::Projective>::new(&a_chunks, &r);
+        let honest_prover = Prover::new(&proof_parameters, &statement, &valid_witness);
+        let valid_proof = honest_prover.prove(rng);
+
+        assert_eq!(Ok(()), valid_proof.verify(&proof_parameters, &statement));
+
+        let new_random_scalars = ScalarSampler::<starknet_curve::Projective>::sample_vector(rng, m*n);
+        let bad_a_chunks = new_random_scalars.chunks(n).map(|c| c.to_vec()).collect::<Vec<_>>();
+
+        let bad_r = ScalarSampler::<starknet_curve::Projective>::sample_vector(rng, a_chunks.len());
+        let bad_a_commits = a_chunks.iter().zip(bad_r.iter()).map(|(a_chunk, &random)|{
+            PedersenCommitment::commit_vector(&commit_key, a_chunk, random)
+        }).collect::<Vec<starknet_curve::Projective>>();
+
+        let statement = Statement::new(&bad_a_commits, raw_prod);
+
+        let invalid_witness = Witness::<starknet_curve::Projective>::new(&bad_a_chunks, &r);
+        let malicious_prover = Prover::new(&proof_parameters, &statement, &invalid_witness);
+        let invalid_proof = malicious_prover.prove(rng);
+
+        assert_ne!(Ok(()), invalid_proof.verify(&proof_parameters, &statement));
+
+        let fake_product = ScalarSampler::<starknet_curve::Projective>::sample_element(rng);
+
+        let wrong_statement = Statement::new(&a_commits, fake_product);
+
+        let invalid_witness = Witness::<starknet_curve::Projective>::new(&a_chunks, &r);
+        let malicious_prover = Prover::new(&proof_parameters, &wrong_statement, &invalid_witness);
+        let invalid_proof = malicious_prover.prove(rng);
+
+        assert_ne!(Ok(()), invalid_proof.verify(&proof_parameters, &wrong_statement));
+    }
+}
