4e57c580619b3596c096bbf5cb0d346f33bab5c1
Andrija <akinovak@gmail.com>
2022-03-07 21:53:47 +0100

    single value argument
    Co-authored-by: Nicolas Mohnblatt <nmohnblatt@users.noreply.github.com>

--- shuffle/src/product_argument/single_value_product_argument/prover.rs
+++ shuffle/src/product_argument/single_value_product_argument/prover.rs
@@ -1,95 +1,115 @@
 use ark_ec::ProjectiveCurve;
 use ark_ff::{Zero, One};
 
-use ark_std::UniformRand;
 use std::iter;
 use merlin::Transcript;
+use rand::Rng;
 
-use crate::utils::{HomomorphicCommitment, PedersenCommitment};
-use crate::product_argument::single_value_product_argument::proof::Proof;
-use crate::transcript::TranscriptProtocol;
+use crate::{
+    utils::{
+        ScalarSampler, RandomSampler, PedersenCommitment, HomomorphicCommitment}, 
+    transcript::TranscriptProtocol,
+    product_argument::single_value_product_argument::{Statement, Witness, Parameters, proof::Proof},
+};
 
-use std::marker::PhantomData;
-
-pub struct Prover<C, const SIZE: usize> 
+pub struct Prover<'a, C>
 where 
-    C: ProjectiveCurve
+    C: ProjectiveCurve,
 {
-    phantom: PhantomData<C>
+    parameters: &'a Parameters<'a, C>,
+    transcript: Transcript,
+    statement: &'a Statement<C>,
+    witness: &'a Witness<'a, C>, 
 }
 
-impl<C, const SIZE: usize> Prover<C, SIZE> 
+impl<'a, C> Prover<'a, C> 
 where   
     C: ProjectiveCurve
 {
-    pub fn create_proof(commit_key: &Vec<C::Affine> ,transcript: &mut Transcript, a: Vec<C::ScalarField>, a_commit: C, r_a: C::ScalarField) -> Proof<C, SIZE> {
-        let mut rng = ark_std::rand::thread_rng();
+    pub fn new(
+        parameters: &'a Parameters<'a, C>,
+        statement: &'a Statement<C>,
+        witness: &'a Witness<'a, C>
+    ) -> Self {
 
-        // let r_a = C::ScalarField::rand(&mut rng);
-        // let a_commit = commit::<C>(&commit_key, &a, r_a);
-        transcript.append(b"a_commit", &a_commit);
+        Self {
+            parameters, 
+            transcript: Transcript::new(b"single_value_product_argument"),
+            statement, 
+            witness
+        }
+    }
+    
+    pub fn prove<R: Rng>(&self, rng: &mut R) -> Proof<C> {
+        let mut transcript = self.transcript.clone();
 
         // generate vector b
-        let bs: Vec<C::ScalarField> = iter::once(a[0])
-        .chain(a.iter().skip(1).scan(a[0], |st, elem| {
+        let b: Vec<C::ScalarField> = iter::once(self.witness.a[0])
+        .chain(self.witness.a.iter().skip(1).scan(self.witness.a[0], |st, elem| {
             *st *= elem;
             Some(*st)
         }))
         .collect();
 
-        // sample d1..dn & delta_2..delta_n-1
-        let mut ds = vec![C::ScalarField::zero(); SIZE];
-        let mut deltas = vec![C::ScalarField::zero(); SIZE];
-        for i in 0..SIZE {
-            ds[i] = C::ScalarField::rand(&mut rng);
-            deltas[i] = C::ScalarField::rand(&mut rng);
-        }
-
-        deltas[0] = ds[0];
-        deltas[SIZE - 1] = C::ScalarField::zero();
+        let d = ScalarSampler::<C>::sample_vector(rng, self.parameters.n);
+        let mut deltas = ScalarSampler::<C>::sample_vector(rng, self.parameters.n - 2);
+        deltas.insert(0, d[0]);
+        deltas.push(C::ScalarField::zero());
 
         // // sample rd
-        let r_d = C::ScalarField::rand(&mut rng);
+        let r_d = ScalarSampler::<C>::sample_element(rng);
 
         // sample s1, sx
-        let s_1 = C::ScalarField::rand(&mut rng);
-        let s_x = C::ScalarField::rand(&mut rng);
+        let s_1 = ScalarSampler::<C>::sample_element(rng);
+        let s_x = ScalarSampler::<C>::sample_element(rng);
 
-        let d_commit = PedersenCommitment::<C>::commit_vector(&commit_key, &ds, r_d);
+        let d_commit = PedersenCommitment::<C>::commit_vector(&self.parameters.commit_key, &d, r_d);
 
         let minus_one = -C::ScalarField::one();
-        let delta_ds = deltas.split_last().unwrap().1.iter().zip(ds.iter().skip(1)).map(|(delta, d)| {
+        let delta_ds = deltas.iter().take(deltas.len() - 1).zip(d.iter().skip(1)).map(|(delta, d)| {
             minus_one * delta * d
         }).collect::<Vec<_>>();
 
-        let delta_ds_commit = PedersenCommitment::<C>::commit_vector(&commit_key, &delta_ds, s_1);
+        let delta_commit = PedersenCommitment::<C>::commit_vector(&self.parameters.commit_key, &delta_ds, s_1);
 
-        let mut diffs = vec![C::ScalarField::zero(); SIZE - 1];
 
-        //TODO implement this with windows&iters
-        for i in 0..SIZE-1 {
-            diffs[i] = deltas[i + 1] - a[i + 1]*deltas[i] - bs[i]*ds[i + 1]
-        }
+        // skip frist a, skip first d, skip last b, and use all deltas
+        let diffs = self.witness.a.iter().skip(1)
+            .zip(d.iter().skip(1))
+            .zip(b.iter().take(b.len() - 1))
+            .zip(deltas.iter().skip(1))
+            .zip(deltas.iter().take(deltas.len() - 1))
+            .map(|((((&a_i, &d_i), &b_i_minus_one), &delta_i), &delta_i_minus_1)| {
+                delta_i + minus_one*a_i*delta_i_minus_1 + minus_one*b_i_minus_one*d_i
+            }).collect::<Vec<_>>();
 
-        let diff_commit = PedersenCommitment::<C>::commit_vector(&commit_key, &diffs, s_x);
 
+        let diff_commit = PedersenCommitment::<C>::commit_vector(&self.parameters.commit_key, &diffs, s_x);
+
+        //public information
+        transcript.append(b"commit_key", self.parameters.commit_key);
+        transcript.append(b"a_commit", &self.statement.a_commit);
+
+        //commits
         transcript.append(b"d_commit", &d_commit);
-        transcript.append(b"delta_ds_commit", &delta_ds_commit);
+        transcript.append(b"delta_commit", &delta_commit);
         transcript.append(b"diff_commit", &diff_commit);
 
         let x = transcript.challenge_scalar(b"x");
 
-        let a_blinded = Self::blind(&a, &ds, x);
-        let r_blinded = x * r_a + r_d;
+        let a_blinded = Self::blind(&self.witness.a, &d, x);
+        let r_blinded = x * self.witness.random_for_a_commit + r_d;
 
-        let b_blinded = Self::blind(&bs, &deltas, x);
+        let b_blinded = Self::blind(&b, &deltas, x);
         let s_blinded = x * s_x + s_1;
 
         Proof {
-            a_commit,
+            // round 1
             d_commit,
-            delta_ds_commit,
+            delta_commit,
             diff_commit,
+
+            // round 2
             a_blinded,
             b_blinded,
             r_blinded,
